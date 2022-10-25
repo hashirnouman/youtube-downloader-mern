29a13a70e887abe3345912ff575859086524fba2
hashirnouman <hashirnouman76@gmail.com>
2022-10-26 03:53:25 +0500

    db connection established

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,22 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
+import * as bodyparser from "body-parser";
+import mongoose from "mongoose";
 dotenv.config();
-
 const app: Express = express();
+app.use(cors());
+app.use(bodyparser.json());
 const port = process.env.PORT;
-
+const uri: any = process.env.URI;
+mongoose
+  .connect(uri)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
