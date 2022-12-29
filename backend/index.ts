76649c3b258477d8919b6fe1c60372a25d1c5b81
hashirnouman <hashirnouman76@gmail.com>
2022-12-30 00:17:52 +0500

    video download issue fixed

--- backend/index.ts
+++ backend/index.ts
@@ -3,26 +3,25 @@ import dotenv from "dotenv";
 import route from "./routes";
 import cors from "cors";
 import * as bodyparser from "body-parser";
-import mongoose from "mongoose";
 import ytdl from "ytdl-core";
 dotenv.config();
 const app: Express = express();
+
 app.use(cors());
 app.use(bodyparser.json());
 const port = process.env.PORT;
-const uri: any = process.env.URI;
-mongoose
-  .connect(uri)
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-    });
-  })
-  .catch((err) => console.log(err));
+
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: false,
   })
 );
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
 app.use("/", route);
--- backend/routes.ts
+++ backend/routes.ts
@@ -1,18 +1,45 @@
 import express from "express";
-import ytdl, { videoInfo } from "ytdl-core";
 
-import * as AdminController from "./controller/AdminController";
+import cors from "cors";
+import ytdl from "ytdl-core";
+import fs from "fs";
+const axios = require("axios");
 const route = express.Router();
-route.post("/post", AdminController.create);
 route.get("/download", async (req, res) => {
   let link: any = req.query.url;
-  res.header("Content-Disposition", 'attachment; filename="video.mp4');
+  const datas = await ytdl.getBasicInfo(link);
+  const title: any = datas.videoDetails.title;
+  console.log(title);
+  res.header("Content-Disposition", `attachment; filename="video.mp4`);
   const str: any = "mp4";
   const id = ytdl.getVideoID(link);
-  console.log(id);
   ytdl(`https://youtu.be/${id}`, {
     format: str,
     filter: "videoandaudio",
   }).pipe(res);
 });
+
+route.post("/view", async (req, res) => {
+  try {
+    const link: any = req.body.link;
+    console.log(link);
+    const resp = await ytdl.getBasicInfo(link);
+
+    res.send({
+      iframe: resp.videoDetails.embed.iframeUrl,
+      link: link,
+    });
+  } catch (error) {
+    console.log("error");
+  }
+});
+route.get("/test", (req, res) => {
+  const url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+  ytdl(url, {
+    filter: "videoandaudio",
+  });
+  res.header("");
+});
+
 export default route;
