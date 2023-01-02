a97ef337ab996d810af5974c203fa64c01a67c57
hashirnouman <hashirnouman76@gmail.com>
2023-01-02 09:09:03 +0500

    showing available quality

--- backend/routes.ts
+++ backend/routes.ts
@@ -1,22 +1,36 @@
 import express from "express";
-
-import cors from "cors";
 import ytdl from "ytdl-core";
-import fs from "fs";
+
 const axios = require("axios");
+const contentDisposition = require("content-disposition");
 const route = express.Router();
 route.get("/download", async (req, res) => {
   let link: any = req.query.url;
+  let format: any = req.query.format;
   const datas = await ytdl.getBasicInfo(link);
   const title: any = datas.videoDetails.title;
-  console.log(title);
-  res.header("Content-Disposition", `attachment; filename="video.mp4`);
-  const str: any = "mp4";
+  const filename = `${title}.mp4`;
+  const audioFilename = `${title}.mp3`;
   const id = ytdl.getVideoID(link);
-  ytdl(`https://youtu.be/${id}`, {
-    format: str,
-    filter: "videoandaudio",
-  }).pipe(res);
+  if (format == "mp4") {
+    res.writeHead(200, {
+      "Content-Disposition": contentDisposition(filename),
+    });
+    const str: any = "mp4";
+    ytdl(`https://youtu.be/${id}`, {
+      format: str,
+      filter: "videoandaudio",
+    }).pipe(res);
+  } else if (format == "mp3") {
+    res.writeHead(200, {
+      "Content-Disposition": contentDisposition(audioFilename),
+    });
+    const str: any = "mp3";
+    ytdl(`https://youtu.be/${id}`, {
+      format: str,
+      filter: "audio",
+    }).pipe(res);
+  }
 });
 
 route.post("/view", async (req, res) => {
@@ -24,7 +38,6 @@ route.post("/view", async (req, res) => {
     const link: any = req.body.link;
     console.log(link);
     const resp = await ytdl.getBasicInfo(link);
-
     res.send({
       iframe: resp.videoDetails.embed.iframeUrl,
       link: link,
@@ -33,13 +46,13 @@ route.post("/view", async (req, res) => {
     console.log("error");
   }
 });
-route.get("/test", (req, res) => {
-  const url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-
-  ytdl(url, {
-    filter: "videoandaudio",
-  });
-  res.header("");
+route.get("/test", async (req, res) => {
+  const link: any = req.query.link;
+  const resp = await ytdl.getInfo(link);
+  const arr = resp.formats
+    .map((val) => val.qualityLabel)
+    .filter((val, index, self) => self.indexOf(val) == index && val != null);
+  res.send(arr);
 });
 
 export default route;
